0df274d7dee7a691d6bffe2c1703eb6e08e4f59e
Leto Rodriguez <leto@leanmind.es>
2024-11-27 12:54:27 +0000

    feat(library): setup library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,42 +1,51 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://letorguez.github.io',
-	base: 'monads-docs',
-	integrations: [
-		starlight({
-			title: 'Monads',
-			social: {
-				github: 'https://github.com/lean-mind/monads',
-			},
-			sidebar: [
-				{
-					label: 'How to use',
-					autogenerate: { directory: 'how-to-use' },
-				},
-				{
-					label: 'Option',
-					autogenerate: { directory: 'option' },
-				},
-				{
-					label: 'Try',
-					autogenerate: { directory: 'try' },
-				},
-				{
-					label: 'Either',
-					autogenerate: { directory: 'either' },
-				},
-				{
-					label: 'IO',
-					autogenerate: { directory: 'io' },
-				},
-				{
-					label: 'Future',
-					autogenerate: { directory: 'future' },
-				},
-			],
-		}),
-	],
+    site: 'https://letorguez.github.io',
+    base: 'monads-docs',
+    integrations: [
+        starlight({
+            title: 'Monads',
+            social: {
+                github: 'https://github.com/lean-mind/monads',
+            },
+            head: [
+                {
+                    tag: 'script',
+                    attrs: {
+                        src: './src/assets/loadLibrary.js',
+                        type: 'module',
+                    },
+                },
+            ],
+            sidebar: [
+                {
+                    label: 'How to use',
+                    autogenerate: {directory: 'how-to-use'},
+                },
+                {
+                    label: 'Option',
+                    autogenerate: {directory: 'option'},
+                },
+                {
+                    label: 'Try',
+                    autogenerate: {directory: 'try'},
+                },
+                {
+                    label: 'Either',
+                    autogenerate: {directory: 'either'},
+                },
+                {
+                    label: 'IO',
+                    autogenerate: {directory: 'io'},
+                },
+                {
+                    label: 'Future',
+                    autogenerate: {directory: 'future'},
+                },
+            ],
+        }),
+    ],
 });
---
+++ src/assets/loadLibrary.js
@@ -0,0 +1,3 @@
+import * as Monads from 'https://unpkg.com/@leanmind/monads@latest/dist/esm/index.js';
+window.Monads = Monads;
+console.info('Type Monads to use the library in devtools console! Enjoy!!!🙌🏻');
